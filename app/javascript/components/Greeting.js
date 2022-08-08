5d8cb2b848f98c4e49327614092d74184e8da92b
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-08-09 02:52:41 +0430

    render greetings

--- app/javascript/components/Greeting.js
+++ app/javascript/components/Greeting.js
@@ -1,12 +1,14 @@
-import React from "react"
-import PropTypes from "prop-types"
-class Greeting extends React.Component {
-  render () {
-    return (
-      <React.Fragment>
-      </React.Fragment>
-    );
-  }
-}
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
-export default Greeting
+const Greeting = () => {
+  const greeting = useSelector((state) => state.greeting);
+
+  return (
+    <div>
+      <h1> {greeting} </h1>
+    </div>
+  );
+};
+
+export default Greeting;
