8b6cffa3254899b6b62017a3a885bf0a177e9dda
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-08-08 22:56:33 +0430

    create api endpoint

---
+++ app/controllers/api/v1/greetings_controller.rb
@@ -0,0 +1,11 @@
+module Api
+    module V1
+      class GreetingsController < ApplicationController
+        def index 
+            greeting = Greeting.find(rand(1..5))
+            render json: greeting, only:[message]   
+        end 
+      end
+    end
+  end
+  
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
 root "messages#index"
+namespace 'api' do
+  namespace 'v1' do
+    get 'greetings' => 'greetings#index'
+  end
+end
 end
