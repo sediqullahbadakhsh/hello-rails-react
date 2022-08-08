566b2301980f0741a51d625365b6460718186e88
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-08-09 02:22:57 +0430

    add data to the database

--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,8 @@ ActiveRecord::Schema.define(version: 2022_08_08_175515) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "messages", force: :cascade do |t|
-    t.string "greeting"
+  create_table "greetings", force: :cascade do |t|
+    t.string "message"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
